670b8d560b8b59e2f2bc116a053f2bbf4244966b
Josh Kasuboski <josh.kasuboski@gmail.com>
2025-06-23 18:41:53 +0200

    Set media type for podman to be happy (#9)
    * allow mediaType to be set
    
    * set content type with mediaType
    
    * put &str back

--- src/manifest.rs
+++ src/manifest.rs
@@ -53,7 +53,7 @@ impl OciManifest {
     /// Returns the appropriate content-type for each variant.
     pub fn content_type(&self) -> &str {
         match self {
-            OciManifest::Image(_) => OCI_IMAGE_MEDIA_TYPE,
+            OciManifest::Image(m) => m.media_type.as_deref().unwrap_or(OCI_IMAGE_MEDIA_TYPE),
             OciManifest::ImageIndex(_) => IMAGE_MANIFEST_LIST_MEDIA_TYPE,
         }
     }
